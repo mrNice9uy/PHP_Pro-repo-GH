bd0ff925bbe96b3150db87cd26f9b51209d90e5d
geekbrains <info@geekbrains.ru>
2021-03-01 05:06:36 +0300

    PHP_Pro-lesson_2-homework

---
+++ models/User.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace models;
+
+class User extends Model
+{
+    protected $id;
+    protected $login;
+    protected $password;
+    protected $email;
+
+    public function getTableName(): string
+    {
+        // TODO: Implement getTableName() method.
+        return 'users';
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLogin()
+    {
+        return $this->login;
+    }
+
+    /**
+     * @param mixed $login
+     */
+    public function setLogin($login): void
+    {
+        $this->login = $login;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+
+}
